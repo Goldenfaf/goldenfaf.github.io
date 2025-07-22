1d61e7d7e11acc51c972b4c7eccec4b11c962d82
Goldenfaf <168614283+Goldenfaf@users.noreply.github.com>
2025-07-21 20:26:18 -0700

    progress
    :3

---
+++ settings.js
@@ -0,0 +1,128 @@
+var name = "Emily Yoon";
+var positions = "P, UTL, 1B";
+
+var blurbInfo = ["2028", "Fullerton, CA", "BSC Bengals - Briggs 14U-R, BSC Bengals", "5'6\"", "115lbs", "4.78/4.83"];
+var blurbInfoType = ["Grad Year", "Hometown", "Club", "Height", "Weight", "GPA(Weighted)"];
+
+var videoAmnt = 4;
+
+var videoTitles = ["Video 1"
+,"Video 2"
+,"Banana"
+,"Video 4aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  a"
+]
+
+var videoDescriptions = ["I like cheeseI like cheeseI like cheeseI like cheeseI like cheeseI like cheeseI like cheeseI like cheeseI like cheese"
+,"Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play Testing paragraph this is the testing paragraph oh the testing paragraph has come out to play"
+,":3"
+,""
+]
+var email = "mailto:emilyyoon0001@gmail.com";
+var twitter = "https://x.com/EmilyYoon2028";
+var instagram = "https://instagram.com/ey1_fastpitch";
+
+var personalStatement = " Hello.  My name is Emily and I have been recognized as one of the best pitchers in the country by USA Softball HPP (top 3), Extra Innings (#2 West), LineDriveMedia (top 10), and others.  I aspire to continue to be part of this select group of talented pitchers through elite determination, work ethic, and competitiveness.  I have been fortunate enough to compete on nationally recognized teams since the age of 9 and have competed against the best teams and best pitchers across the nation.  I am tough and tournament-tested as I have won 5 National Championships (3x PGF, Sparkler, IDT) and have pitched in 4 National Finals games (3-0, twice on TV).  I love softball, baking, and hanging out with my friends and I am ranked near the top of my class with a 4.8 GPA. ";
+
+var g1Name = "Kenny Yoon";
+var g1Email = "mailto:kgyoon@hotmail.com";
+var g1Phone = "tel:2139262518";
+
+var g2Name = "Renee Yoon";
+var g2Email = "mailto:rten1@hotmail.com";
+var g2Phone = "tel:7143964579";
+
+var team1Name = "BSC Bengals - Briggs 14U-R, BSC Bengals";
+var team1Location = "Smots-8";
+var team1Coach = "Roseann Briggs";
+var team1CoachPhone = "Phone: (714) 598-5672";
+var team1CoachEmail = "Email: bscbengals@gmail.com";
+var team1Jersey = "#1";
+
+var team2Name = "El Dorado High School";
+var team2Location = "Placentia, CA";
+var team2Coach = "Melissa Chavez";
+var team2CoachPhone = "Phone: (714) 986-7580";
+var team2CoachEmail = "Email: mchavez@pylusd.org";
+var team2Jersey = "#27";
+
+
+var positionsFull = "Pitcher, Utility, First Base";
+// Height and Weight taken from blurbInfo
+var handednessBatting = "Right";
+var handednessThrowing = "Right";
+
+var pitchingSpeedMax = "64 mph";
+var hittingSpeedMax = "65 mph";
+
+var yards40Sprint = "5.59s";
+var yards20Sprint = "2.97s";
+
+
+var additionalStats = ["https://web.gc.com/athlete/eyfastpitch",
+"https://www.maxpreps.com/ca/placentia/el-dorado-golden-hawks/athletes/emily-yoon/softball/stats/?careerid=1okfc04bk6s44"];
+
+
+var athleticHonors = ["2025 Louisville Slugger IDT 14U Champion - SP, WP, CG, SO"
+,
+        "2025 LineDriveMedia #16 Hot 100 Player Rankings" 
+,
+        "2025 High School Varsity: Scholar-Athlete award, Team MVP, Freeway League Most Valuable Pitcher"
+,
+        "2025 Orange County High School Leaders: #1 Rank ERA (0.54), #7 Rank Strikeouts (155K) in 116.2IP"
+,
+        "2024 Extra Innings #12 National Pitcher Rank.  #2 West Region Pitcher Rank."
+,
+        "2024 USA Softball HPP NSE U-13 Top Performer (1 of 3 pitchers named)"
+,
+        "2023 12U PGF Premier National Championship game - SP, WP, CG, SH"
+,
+        "2023 Extra Innings #13 Position Rank"
+,
+        "2022 12U PGF Platinum National Champ"
+,
+        "2021 10U PGF Premier National Championship game - SP, WP, CG"
+,
+        "2019 8U District, State, and Regional National Championship Starting Pitcher"];
+
+var otherSports = ["I like to bake, draw, play with my dog, and hang out with my friends in my free time."];
+
+
+
+
+
+
+
+
+var pressLinks = ["https://extrainningsoftball.com/pgf-10u-premier-national-championship-ohana-tigers-iwai-knock-off-california-cruisers-carreon-10-1-with-explosive-6-run-third-inning/"
+,"https://www.youtube.com/watch?v=RwEzFC_zkeg&ab_channel=PGFnetwork"
+,"https://extrainningsoftball.com/event-recap-pgf-futures-organizational-challenge-which-featured-many-of-the-wests-best-at-14u-12u/"
+,"https://www.cosparkfire.com/news/yoons-first-career-hr-lifts-bengals-to-12u-title"
+,"https://extrainningsoftball.com/pgf-12u-premier-national-championship-bengals-ckm-caffrey-vs-arizona-storm-kobasew/"
+,"https://www.youtube.com/watch?v=UVOFUfRDBdk&t=2311s"
+];
+
+var pressLinksTitles = ["EIS: PGF 10U Premier National Championship: Ohana Tigers (8/21)"
+,"Youtube: 2021 PGF National Championship 10U Premier (Video Replay)"
+,"EIS Event Recap: PGF Futures Org Challenge Which Featured Many of the West's Best (4/23)"
+,"Cosparkfire: Yoon's First Career HR Lifts Bengals to 12u CO Sparkler Title (7/23)"
+,"EIS: PGF 12U Premier National Championship: Bengals CKM (8/23)"
+,"YouTube: 2023 PGF National Championship 12U Premier (Video Replay-8/23)"
+];
+
+
+var eventNames = ["PGF National Championship"
+,"IDT Softball (2025 Independence Day Softball)"
+];
+var eventLinks = ["https://pgfnationals.com/week-two"
+,"https://boulderidt.com/"
+];
+var eventDates = ["07/27/25 - 08/03/25"
+,"07/02/25 - 07/06/25"
+];
+var eventLocations = ["Huntington Beach"
+,"Boulder"
+];
+
+var classYear = 2028;
+
+var major = null;
